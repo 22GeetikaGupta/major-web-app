b3a350ead871458d23aa2b4e799af1cbd01b698c
Geetika Gupta <22guptageetika@gmail.com>
2021-02-09 18:49:39 +0530

    .

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("project development started");
+var express = require('express')
+var app = express()
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname,'static')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('home.ejs')
+})
+
+app.get('/login', function (req, res) {
+  res.render('LoginForm.ejs')
+})
+
+app.get('/login2', function (req, res) {
+  res.render('login2.ejs')
+})
+
+app.get('/userhome', function (req, res) {
+  res.render('userHome.ejs')
+})
+
+app.get('/userprofile', function (req, res) {
+  res.render('userProfile.ejs')
+})
+
+app.get('/appliedJobs', function (req, res) {
+  res.render('appliedJobs.ejs')
+})
+ 
+app.get('/companyHome', function (req, res) {
+  res.render('HomeCompany.ejs')
+})
+
+app.get('/viewt', function (req, res) {
+  res.render('viewt.ejs')
+})
+app.listen(4000)
---
+++ static/scripts/login2.js
@@ -0,0 +1,9 @@
+function login(){
+	document.getElementsByClassName("form1")[0].style.display = "block";
+	document.getElementsByClassName("register")[0].style.display = "none";
+}
+
+function register(){
+	document.getElementsByClassName("form1")[0].style.display = "none";
+	document.getElementsByClassName("register")[0].style.display = "block";
+}
