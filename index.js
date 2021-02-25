a002376903046a218f55c128369da6c1252057e3
Geetika Gupta <22guptageetika@gmail.com>
2021-02-25 13:06:28 +0530

    .

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var express = require('express')
 var app = express()
 const path = require('path');
 var knex = require('knex');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
-
+var bcrypt = require('bcrypt');
 
 knex = knex({
 	client: 'mysql',
@@ -16,6 +17,12 @@ knex = knex({
 	}
 	
 });
+
+app.use(cookieSession({
+	name: "session",
+	keys: ['key1', 'key2']
+
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -34,14 +41,58 @@ knex('userdetails')
 
 app.set('view engine', 'ejs');
 
+function redirecthome(req, res, next)
+{
+	if(req.session.username)
+	{
+		next();
+		
+	}
+	else
+	{
+		res.redirect("/");
+	}
+}
+
+
 app.get('/', function (req, res) {
-	res.render('home.ejs');
+
+		var username = "";
+		if(req.session.username) username = req.session.username;
+
+		res.render('home.ejs', {username:username});
 	
   
 })
 
 app.get('/login', function (req, res) {
-  res.render('LoginForm.ejs')
+	if(req.session.username) 
+	{
+		res.redirect('/userHome');
+	}
+	else
+  		res.render('LoginForm.ejs', {message : ""});
+})
+
+app.post("/login", function(req, res){
+
+	var obj = req.body;
+	console.log(obj);
+	knex("logintable").select('Username', 'Password').where('Username', '=', obj.username)
+	.then(data => {
+			console.log("hey this is data", data);
+			if(data.length==0 ||data[0].Password != obj.password){
+				res.render('LoginForm.ejs', {message : "Wrong username/password"});
+			}
+			else{
+
+				req.session.username = obj.username;
+				req.session.loggedin = true;
+				res.redirect("/userHome");
+			}
+		
+	})
+	.catch(err=>res.send(err));
 })
 
 app.post("/register", function (req, res){
@@ -83,7 +134,7 @@ app.get('/userprofile', function (req, res) {
   res.render('userProfile.ejs')
 })
 
-app.get('/appliedJobs', function (req, res) {
+app.get('/appliedJobs', redirecthome, function (req, res) {
   res.render('appliedJobs.ejs')
 })
  
@@ -93,5 +144,10 @@ app.get('/companyHome', function (req, res) {
 
 app.get('/viewt', function (req, res) {
   res.render('viewt.ejs')
+})
+
+app.get('/logout', function (req, res) {
+  req.session = null;
+  res.redirect('/');
 })
 app.listen(4000)
